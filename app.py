0aa245a8d2016e073cea176e534f1e8d38ffe27d
sasmita-btech <saisami333@gmail.com>
2026-04-14 15:22:25 +0530

    Add PDF loading functionality to app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("AI Project using Endee - RAG Demo") 
+from langchain.document_loaders import PyPDFLoader
+
+def load_pdf(file_path):
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+    return docs
+
+if __name__ == "__main__":
+    print("AI PDF Question Answering using Endee")
+
+    file_path = "sample.pdf"   # keep a PDF in same folder
+    try:
+        documents = load_pdf(file_path)
+        print("PDF Loaded Successfully!")
+        print(f"Total Pages: {len(documents)}")
+    except:
+        print("Please add a PDF file named sample.pdf")
